6189ab0e5b92de9e7cc02f90be0f30c4e89327d9
Punisher <palekseev102@gmail.com>
2026-02-15 10:04:34 +0300

    feat(analyzer): add inspection for ast files (nodes)

--- analyzer.go
+++ analyzer.go
@@ -1,6 +1,10 @@
 package loglint
 
-import "golang.org/x/tools/go/analysis"
+import (
+	"go/ast"
+
+	"golang.org/x/tools/go/analysis"
+)
 
 var Analyzer = &analysis.Analyzer{
 	Name: "loglint",                                         // имя линтера
@@ -9,5 +13,14 @@ var Analyzer = &analysis.Analyzer{
 }
 
 func run(pass *analysis.Pass) (any, error) {
+	for _, file := range pass.Files {
+		ast.Inspect(file, func(n ast.Node) bool {
+			return ins(n, pass)
+		})
+	}
+	return nil, nil
+}
+
+func ins(n ast.Node, pass *analysis.Pass) bool {
 
 }
