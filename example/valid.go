d9c92b4e10531f885b2b6e0189315435cd566e6b
Punisher <palekseev102@gmail.com>
2026-02-17 13:31:15 +0300

    refactor(test): remove % from message

--- example/valid.go
+++ example/valid.go
@@ -12,5 +12,5 @@ func valid() {
 	void := validVoid{
 		smth: "smth",
 	}
-	zap.L().Info("info - %v", zap.String("void smth", void.smth))
+	zap.L().Info("info - ", zap.String("void smth", void.smth))
 }
